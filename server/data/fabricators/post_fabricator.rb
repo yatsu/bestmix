41b1cf071ee62c776bd739fdef2b16f328b6f05f
Masaki Yatsu <yatsu@yatsu.info>
2012-08-13 19:40:53 +0900

    [server] Modified sample data generator to use Factory Girl.

--- server/data/fabricators/post_fabricator.rb
+++
@@ -1,8 +0,0 @@
-Fabricator(:post) do
-  title { Faker::Lorem.sentence }
-  content { Faker::Lorem.paragraphs(rand(9) + 1).join("\n\n") }
-  publish { rand(2) == 0 ? "true" : "false" }
-  created_at { sequence(:created, 1).hours.ago.utc }
-  updated_at { sequence(:updated, 1).hours.ago.utc }
-  user_id { rand(5) + 1 }
-end
--- server/script/generate_posts.rb
+++ server/script/generate_posts.rb
@@ -1,3 +1,18 @@
+require 'factory_girl'
+require 'ffaker'
+
+FactoryGirl.define do
+  factory :post do
+    title { Faker::Lorem.sentence }
+    content { Faker::Lorem.paragraphs }
+    sequence(:published_at) { |n| n.days.ago.utc }
+    publish { rand(2) == 0 ? "true" : "false" }
+    sequence(:created_at) { |n| n.hours.ago.utc }
+    sequence(:updated_at) { |n| n.hours.ago.utc }
+    user_id { rand(5) + 1 }
+  end
+end
+
 for i in 0..255
-  Fabricate(:post)
+  FactoryGirl.create(:post)
 end
